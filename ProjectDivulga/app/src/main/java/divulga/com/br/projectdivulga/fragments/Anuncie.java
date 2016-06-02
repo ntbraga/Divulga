9c0c86f82525e7f91569cf0b26dd023cfcff4422
Pedro Neto <ntbraga@live.com>
2016-06-02 17:23:35 -0300

    version 1.2

--- ProjectDivulga/app/src/main/java/divulga/com/br/projectdivulga/fragments/Anuncie.java
+++ ProjectDivulga/app/src/main/java/divulga/com/br/projectdivulga/fragments/Anuncie.java
@@ -3,6 +3,7 @@ package divulga.com.br.projectdivulga.fragments;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,17 +12,21 @@ import android.view.ViewGroup;
 import divulga.com.br.projectdivulga.MainActivity;
 import divulga.com.br.projectdivulga.R;
 public class Anuncie extends Fragment {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
+    private FloatingActionButton fab;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         MainActivity.mainActivity.toolbar.setTitle(getString(R.string.anuncie));
-        return inflater.inflate(R.layout.fragment_anuncie, container, false);
+        View view = inflater.inflate(R.layout.fragment_anuncie, container, false);
+        fab = (FloatingActionButton) view.findViewById(R.id.anuncie_fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        return view;
     }
 
 }
